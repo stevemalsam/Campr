71c171f8392aa3eaa08b739c343991de0fcc7695
Steve Malsam <stevemalsam@stevemalsam.com>
2019-11-19 21:16:22 -0800

    Center map around Yellowstone

--- Campy/ViewController.swift
+++ Campy/ViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2019 Steve Malsam. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
-class ViewController: UIViewController {
+// 44.4280° N, 110.5885° W - Coordinates of Yellowstone
+let YELLOWSTONE_COORDINATES = CLLocationCoordinate2DMake(44.43, -110.52)
+let YELLOWSTONE_SPAN = MKCoordinateSpan(latitudeDelta: 1.6, longitudeDelta: 1.2)
 
+class ViewController: UIViewController {
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let yellowstoneRegion = MKCoordinateRegion(center: YELLOWSTONE_COORDINATES, span: YELLOWSTONE_SPAN)
+        
+        self.map.setRegion(yellowstoneRegion, animated: false)
     }
+}
 
-
+extension ViewController:MKMapViewDelegate {
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        print("Visible Region: \(mapView.region)")
+    }
 }
 
